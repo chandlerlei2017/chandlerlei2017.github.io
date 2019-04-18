a9744131aeaaf723694b7cd650a8b685b7aac148
chandlerlei2017 <chandlerlei87@gmail.com>
2019-04-17 22:32:45 -0400

    reinstall dependecies

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import './custom.scss'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class App extends Component {
