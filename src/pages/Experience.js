bc16f57091aba4dfc85d38806ab1d6af1a87b4d5
chandlerlei2017 <chandlerlei87@gmail.com>
2019-04-19 15:33:28 -0400

    add experience placeholders

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -9,14 +9,57 @@ function Experience() {
           <p>Here is my coding experience along with some of the work I am most proud of!</p>
         </div>
       </div>
+
       <div className="p-5 white">
-        <h2>Work Experience</h2>
+        <h2 className="mb-5">Work Experience</h2>
+
+        <div className="mb-5">
+          <h5>Lending Loop</h5>
+          <h6>Software Developer</h6>
+        </div>
+
+        <div className="mb-5">
+          <h5>Bank of Montreal</h5>
+          <h6>Infrastructure Engineer</h6>
+        </div>
       </div>
-      <div className="p-5 green">
-        <h2>Projects</h2>
+
+      <div className="p-5">
+        <h2 className="mb-5">Projects</h2>
+
+        <div className="mb-5">
+          <h5>This Website!</h5>
+          <h6>Yes I am including this website as a presonal project</h6>
+        </div>
+
+        <div className="mb-5">
+          <h5>Gmail Chrome Extension</h5>
+          <h6>Chrome extension providing the functionality of Gmail</h6>
+        </div>
+
+        <div className="mb-5">
+          <h5>Quizit</h5>
+          <h6>Android Quiz App</h6>
+        </div>
+
+        <div className="mb-5">
+          <h5>Light Tricks</h5>
+          <h6>Onion Omega2 LED multi-tool</h6>
+        </div>
       </div>
+
       <div className="p-5 white">
-        <h2>Competitions/Hackathons</h2>
+        <h2 className="mb-5">Competitions/Hackathons</h2>
+
+        <div className="mb-5">
+          <h5>Business Development Bank of Canada Hackathon</h5>
+          <h6>March 2019</h6>
+        </div>
+
+        <div className="mb-5">
+          <h5>Big Data Challenge</h5>
+          <h6>Februrary 2017</h6>
+        </div>
       </div>
     </div>
   );
