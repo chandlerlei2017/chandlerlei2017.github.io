411f62506701e89236bf5836f080730fccddb00d
chandlerlei2017 <chandlerlei87@gmail.com>
2019-04-18 23:06:08 -0400

    layout for work experience section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,48 +10,78 @@ function Home() {
           <p>Pushing the Limits</p>
         </div>
       </div>
+
       <div className = "text-center p-5 white">
         <p>"It does not matter how slowly you go as long as you do not stop." Me: 2018</p>
       </div>
+
       <div className = "text-center p-5 white">
         <h2>About Me</h2>
         <p>Who am I? What Do I like to do? Learn More...</p><br/><br/><br/><br/>
       </div>
+
       <div className = "text-center p-5">
         <h2>Projects</h2>
         <p>These are my current projects. Learn more...</p>
         <div className = "row">
           <div className="col-sm-3">
-            <div className="form">
+            <div className="form white">
               <p>This is a test to seee how this element will look </p>
             </div>
           </div>
           <div className="col-sm-3">
-            <div className="form">
+            <div className="form white">
               <p>This is a test to seee how this element will look </p><br/>
               <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
             </div>
           </div>
           <div className="col-sm-3">
-            <div className="form">
+            <div className="form white">
               <p>This is a test to seee how this element will look </p>
             </div>
           </div>
           <div className="col-sm-3">
-            <div className="form">
+            <div className="form white">
               <p>This is a test to seee how this element will look </p>
             </div>
           </div>
         </div>
+
       </div>
       <div className = "text-center p-5 white">
         <h2>Work Experience</h2>
-        <p>My work expreience. Learn more...</p><br/><br/><br/><br/>
+        <p>My work expreience. Learn more...</p>
+        <div className = "row">
+          <div className="col-sm-3">
+            <div className="form green">
+              <p>This is a test to seee how this element will look </p>
+            </div>
+          </div>
+          <div className="col-sm-3">
+            <div className="form green">
+              <p>This is a test to seee how this element will look </p><br/>
+              <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+            </div>
+          </div>
+          <div className="col-sm-3">
+            <div className="form green">
+              <p>This is a test to seee how this element will look </p>
+            </div>
+          </div>
+          <div className="col-sm-3">
+            <div className="form green">
+              <p>This is a test to seee how this element will look </p>
+            </div>
+          </div>
+        </div>
+
       </div>
+
       <div className = "text-center p-5">
         <h2>Contact Me</h2>
         <p>Questions? or just want to chat? Contact me...</p><br/><br/><br/><br/>
       </div>
+
     </div>
   );
 }
