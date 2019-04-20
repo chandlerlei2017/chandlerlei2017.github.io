1415f966f1e20743a45fe822337049e29d5884b2
chandlerlei2017 <chandlerlei87@gmail.com>
2019-04-20 14:04:12 -0400

    scroll effect for contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 function Contact() {
   return (
     <div>
       <div className="hero-image hero-contact">
         <div className="hero-text">
-          <h1>Contact Me</h1>
-          <p>Free to chat!</p>
+          <h1 className="mb-5">Contact Me</h1>
+          <p>Email: chandlerlei87@gmail.com</p>
+          <p>Phone: 647-974-2196</p>
+          <button className="btn btn-primary-white" id="click">Or...</button>
         </div>
       </div>
+      <h3 className="p-5" id="scroll-to">Temporary Stuff....</h3>
+      <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
     </div>
   );
 }
 
 export default Contact;
+
+$(document).ready(function (){
+  $("#click").click(function (){
+      $('html, body').animate({
+          scrollTop: $("#scroll-to").offset().top
+      }, 1000);
+  });
+});
