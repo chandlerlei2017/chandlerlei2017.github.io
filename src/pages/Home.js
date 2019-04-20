0caac0821ac4766b2824a9db5a526c3697a057a5
chandlerlei2017 <chandlerlei87@gmail.com>
2019-04-20 12:45:00 -0400

    header link transitions

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,8 +7,8 @@ function Home() {
     <div>
       <div className="hero-image hero-home">
         <div className="hero-text">
-          <h1>Chandler Lei</h1>
-          <p>Pushing Limits</p>
+          <h1>Chandler Lei (雷迁)</h1>
+          <p>Pushing the limits</p>
           <Link to="/about" className="btn btn-primary-white">About Me</Link>
         </div>
       </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 function Projects() {
   return (
     <div>
-      <div className="hero-image">
+      <div className="hero-image hero-projects">
         <div className="hero-text">
           <h1>Projects</h1>
           <p>Here are some examples of the work I do</p>
