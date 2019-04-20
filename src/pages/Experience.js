97a9f40543953a512bfd940da1bedac0727a926e
chandlerlei2017 <chandlerlei87@gmail.com>
2019-04-20 11:22:16 -0400

    add employment card styling

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -11,39 +11,77 @@ function Experience() {
         </div>
       </div>
 
-      <div className="p-5 white">
-        <h2 className="mb-5">Work Experience</h2>
+      <div className="p-5">
+        <h2 className="text-center">Work Experience</h2>
+        <p className="text-center">These are some of the places I worked!</p>
+        <div className="col-sm-10 offset-sm-1 mb-5">
+          <div className="row">
+            <div className="col-sm-8 pr-2 mb-5">
+             <div className="form p-3 white">
+               <h3 className="ll-colour">Lending Loop</h3>
+               <hr className="ll-divider mb-4"/>
 
-        <div className="mb-5">
-          <h4>Lending Loop</h4>
-          <h6>Software Developer</h6>
-          <h6>January - April 2019</h6>
-        </div>
+               <h5>What I did there</h5>
+               <p>This will either be a paragraph or a "ul" of my job responsibilities</p>
+               <br/><br/><br/><br/>
 
-        <div className="mb-5">
-          <h4>Bank of Montreal</h4>
-          <h6>Infrastructure Engineer</h6>
-          <h6>May - Augutst 2019</h6>
+               <h5>Technologies used</h5>
+               <p>This will either be a paragraph or a "ul" of the tech I used</p>
+               <br/><br/><br/><br/>
+             </div>
+            </div>
+
+            <div className="col-sm-4 pl-2 mb-5 employment">
+              <div className="form ll-bg text-center p-2">
+                  <p className="mb-0 white-text">Software Developer</p>
+              </div>
+              <div className="form p-3 white">
+              </div>
+            </div>
+
+            <div className="col-sm-4 pr-2 mb-5 employment">
+              <div className="form ll-bg text-center p-2">
+                  <p className="mb-0 white-text">Infrastructure Engineer</p>
+              </div>
+              <div className="form p-3 white">
+              </div>
+            </div>
+
+            <div className="col-sm-8 pl-2 mb-5">
+             <div className="form p-3 white">
+               <h3 className="ll-colour">Bank of Montreal</h3>
+               <hr className="ll-divider mb-4"/>
+
+               <h5>What I did there</h5>
+               <p>This will either be a paragraph or a "ul" of my job responsibilities</p>
+               <br/><br/><br/><br/>
+
+               <h5>Technologies used</h5>
+               <p>This will either be a paragraph or a "ul" of the tech I used</p>
+               <br/><br/><br/><br/>
+             </div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className = "text-center p-5">
+      <div className = "text-center p-5 white">
         <h2>Projects</h2>
         <p>These are my current projects. Learn more...</p>
         <div className = "row mb-5">
           <div className="col-sm-4">
-            <div className="form white p-3 ml-3 mr-3 transition-3d-hover">
+            <div className="project-card white p-3 ml-3 mr-3 transition-3d-hover">
               <h5>Gmail Chrome Extension</h5>
             </div>
           </div>
           <div className="col-sm-4">
-            <div className="form white p-3 ml-3 mr-3 transition-3d-hover">
+            <div className="project-card white p-3 ml-3 mr-3 transition-3d-hover">
               <h5>Quizit</h5><br/>
               <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
             </div>
           </div>
           <div className="col-sm-4">
-            <div className="form white p-3 ml-3 mr-3 transition-3d-hover">
+            <div className="project-card white p-3 ml-3 mr-3 transition-3d-hover">
               <h5>Light Tricks</h5>
             </div>
           </div>
@@ -51,7 +89,7 @@ function Experience() {
         <Link to="/projects" className="btn btn-primary-blue">Learn More</Link>
       </div>
 
-      <div className="p-5 white">
+      <div className="p-5">
         <h2 className="mb-5">Competitions/Hackathons</h2>
 
         <div className="mb-5">
