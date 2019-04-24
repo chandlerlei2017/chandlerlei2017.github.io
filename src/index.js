543aa2485c4d7557ee9d103a30095de722593e2a
chandlerlei2017 <chandlerlei87@gmail.com>
2019-04-23 22:59:32 -0400

    parallax scrolling and disappearing text

--- src/index.js
+++ src/index.js
@@ -4,8 +4,21 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import './custom.scss';
+import $ from 'jquery';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+$(document).ready(function(){
+  $(window).scroll(function(e){
+    parallax();
+  });
+  function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.hero-image').css('top',-(scrolled*0.0115)+'rem');
+    $('.hero-image > .hero-text').css('top',(60 + scrolled*.05) + '%');
+    $('.hero-image > .hero-text').css('opacity',1-(scrolled*.003));
+  };
+});
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
