e3549b8274e4ab905998dfe92fd8d5168ad67a39
chandlerlei2017 <chandlerlei87@gmail.com>
2019-05-14 01:16:06 -0400

    add resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import { pdfjs } from 'react-pdf';
+import { Document, Page } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function Resume() {
   return (
-    <div>
-
+    <div className="pt-5 mt-5 pb-5 white">
+      <div className="col-sm-6 offset-sm-3">
+        <a href='/2BResume.pdf' className="btn btn-primary-blue mb-3" download>Download Me!</a>
+        <Document file="2BResume.pdf">
+        <Page pageNumber={1} />
+        </Document>
+      </div>
     </div>
   );
 }
