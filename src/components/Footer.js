e05ed63e4a592ee3f20edd8d980bcdf157137945
chandlerlei2017 <chandlerlei87@gmail.com>
2019-05-18 00:57:18 -0400

    add footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 class Footer extends Component {
   render() {
     return (
-      <footer className="text-right pr-4 pt-1 pb-1 footer">
-        <span className="pr-1"> <FontAwesomeIcon icon={['fab', 'github']} /> </span>
-        <span className="pl-1 pr-1"> <FontAwesomeIcon icon={['fab', 'linkedin']} /> </span>
-        <span className="pl-1"> <FontAwesomeIcon icon="envelope"/> </span>
+      <footer className="text-right pr-4 pt-2 pb-2 footer">
+        <a className="pr-2 footer-icon" href="https://github.com/chandlerlei2017" target="_blank"> <FontAwesomeIcon icon={['fab', 'github']} size='lg'/> </a>
+        <a className="pl-2 pr-2 footer-icon" href="https://www.linkedin.com/in/qian-chandler-l-486069137/" target="_blank"> <FontAwesomeIcon icon={['fab', 'linkedin']} size='lg'/> </a>
+        <a className="pl-2 footer-icon" href="mailto:q4lei@uwaterloo.ca"> <FontAwesomeIcon icon="envelope" size='lg'/> </a>
       </footer>
     );
   }
